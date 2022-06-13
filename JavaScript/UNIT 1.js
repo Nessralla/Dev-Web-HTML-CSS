71808c2fa83eaae666f6d42933ef91e2e7832575
Nessralla <alexandre.nessralla@gmail.com>
2022-06-13 15:33:35 -0300

    aulas html

---
+++ JavaScript/UNIT 1.js
@@ -0,0 +1,8 @@
+//  Três maneiras de de utilizar o javascript:
+//      - dentro do html, utilizando a tag <script type="text/javascript"></script>
+//      - em head, inserir script: <script src="arquivo.js" defer></script>
+//      - Ou ir em inspecionar e no console, normalmente é utilizado para testes
+
+var a = 1
+var b = a + 1
+alert(b)
