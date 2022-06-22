a043f306392db9e3572bfcc056218f38e3f3db65
Nessralla <alexandre.nessralla@gmail.com>
2022-06-22 14:48:08 -0300

    javascript

--- JavaScript/UNIT 1.js
+++ JavaScript/UNIT 1.js
@@ -6,3 +6,24 @@
 var a = 1
 var b = a + 1
 alert(b)
+
+// As variáveis em Javascript podem ter diferentes tipos de dados: Number, String, Boolean, Object, Function
+// Undefined (valor indefinido, ou seja, não foi inicializada) e Null (valor nulo)
+
+// typeof() retorna o tipo da variável
+
+// Operador de atribuição =
+
+// Operadores aritimeticos: + - / * e % (resto da divisão)
+
+// Operadores de comparação
+    // === comparador de igualdade com tipagem
+    // == comparador de igualdade sem tipagem
+    // > , < , <= , >= (maior, menor, menor igual e maior igual)
+    // !== comparador de desigualdade com tipagem
+    // != comparador de desiguldade sem tipagem
+
+// Operadores lógicos: && AND e || OR
+
+// Operadores de atribuição com operação: +=, -= , *= , /= (igual ao Python)
+
---
+++ JavaScript/UNIT 2.js
@@ -0,0 +1,5 @@
+// UNIT 11 - Estruturas de Controle
+
+// O método getElementById() é responsável por retornar o elemento que possui o atributo ID com o valor específicado
+//  var valorInput = getElementById("meu input").value
+
